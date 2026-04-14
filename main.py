011f47a683c3f97f96a11fafd300eb9432ac4b9f
Mila <milaz3388@gmail.com>
2026-04-14 21:55:06 +0800

    把main降低安全性

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 import urllib3
 
 # 關閉因安全性降低而產生的警告訊息
-# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def run_scraper():
     url = "https://example.com"
     try:
         # 加入 verify=False 略過憑證檢查
-        # response = requests.get(url, verify=False)
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
+        # response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         title = soup.find('h1').text
         print(f"成功抓取網頁標題123: {title}")
